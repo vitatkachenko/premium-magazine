35636af4796e39c665d9d2502ea1a44064b896ec
Vita Tkachenko <vita.tkachenko19@gmail.com>
2016-08-11 12:50:27 +0300

    first commit

---
+++ learn-jquery.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	$('.article-image-presentation__title').mouseenter(function() {
+		$(this).addClass("change_color");
+	});
+
+	$('.article-image-presentation__title').mouseleave(function() {
+		$(this).removeClass('change_color');
+	});
+
+	$('.post-identificator__item').mouseenter (
+	function() {
+		$(this).css("text-decoration", "underline");
+	}).mouseleave (
+		function() {
+		$(this).css("text-decoration", "none");
+	});
+
+	/*drop-down menu*/
+
+	$(function(){
+		$('.nav-list .nav-list__item').hover(function(){
+			$(this).children('div.nav-lists').stop(false, true).slideDown(300);
+			$(this).children('a.shop-link').stop(false, true).slideDown(300);
+		}, function(){
+			$(this).children('div.nav-lists').stop(false, true).slideUp(300);
+			$(this).children('a.shop-link').stop(false, true).slideUp(300);
+		});
+	});
+
+	/*slider*/
+	
+	$('a.film_roll_next').click(function(){
+		$('#carousel-wrapper').addClass('.simpleMove');
+	});
+});
+
