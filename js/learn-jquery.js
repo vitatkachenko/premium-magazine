177de0dd2d017add7c28293f02acade8f3a3fabb
Vita Tkachenko <vita.tkachenko19@gmail.com>
2016-08-19 13:14:35 +0300

    last changes

--- js/learn-jquery.js
+++ js/learn-jquery.js
@@ -24,5 +24,113 @@ $(document).ready(function() {
 			$(this).children('a.shop-link').stop(false, true).slideUp(300);
 		});
 	});
+
+	/*сделать кнопки соц.сетей светлее по наведению и выделить курсором*/
+
+	$(function() {
+		$('.fa-rss-square').hover(function(){
+			$(this).addClass("lighten-rss-icon");
+			$(this).css("cursor", "pointer");
+		}, function() {
+			$(this).removeClass("lighten-rss-icon");
+			$(this).css("cursor", "none");
+		});
+	});
+
+	$(function() {
+		$('.fa-facebook-square').hover(function(){
+			$(this).addClass("lighten-facebook-icon");
+			$(this).css("cursor", "pointer");
+		}, function() {
+			$(this).removeClass("lighten-facebook-icon");
+			$(this).css("cursor", "none");
+		});
+	});
+
+	$(function() {
+		$('.fa-twitter-square').hover(function(){
+			$(this).addClass("lighten-twitter-icon");
+			$(this).css("cursor", "pointer");
+		}, function() {
+			$(this).removeClass("lighten-twitter-icon");
+			$(this).css("cursor", "none");
+		});
+	});
+
+	$(function() {
+		$('.fa-google-plus-square').hover(function(){
+			$(this).addClass("lighten-google-icon");
+			$(this).css("cursor", "pointer");
+		}, function() {
+			$(this).removeClass("lighten-google-icon");
+			$(this).css("cursor", "none");
+		});
+	});
+
+	/*для первого комментария*/
+
+	$(function() {
+		$('.carousel-recent-comment-1.first-top-commentator').mouseenter(function(){
+			$(this).css("display", "none");
+			$(this).css("cursor", "pointer");
+			$('.carousel-recent-comment-2.first-top-commentator').css("cursor", "pointer");
+			$('.carousel-recent-comment-2.first-top-commentator').css("display", "inline-block");
+		});
+	});
+
+	$(function() {
+		$('.carousel-recent-comment-2.first-top-commentator').mouseleave(function() {
+			$('.carousel-recent-comment-1.first-top-commentator').css("display", "inline-block");
+			$('.carousel-recent-comment-2.first-top-commentator').css("display", "none");	
+		});
+	});
+
+	/*для второго комментария*/
+
+	$(function() {
+		$('.carousel-recent-comment-1.second-top-commentator').mouseenter(function(){
+			$(this).css("display", "none");
+			$(this).css("cursor", "pointer");
+			$('.carousel-recent-comment-2.second-top-commentator').css("cursor", "pointer");
+			$('.carousel-recent-comment-2.second-top-commentator').css("display", "inline-block");
+		});
+	});
+
+	$(function() {
+		$('.carousel-recent-comment-2.second-top-commentator').mouseleave(function() {
+			$('.carousel-recent-comment-1.second-top-commentator').css("display", "inline-block");
+			$('.carousel-recent-comment-2.second-top-commentator').css("display", "none");	
+		});
+	});
+
+	/*для третьего комментария*/
+
+	$(function() {
+		$('.carousel-recent-comment-1.third-top-commentator').mouseenter(function(){
+			$(this).css("display", "none");
+			$(this).css("cursor", "pointer");
+			$('.carousel-recent-comment-2.third-top-commentator').css("cursor", "pointer");
+			$('.carousel-recent-comment-2.third-top-commentator').css("display", "inline-block");
+		});
+	});
+
+	$(function() {
+		$('.carousel-recent-comment-2.third-top-commentator').mouseleave(function() {
+			$('.carousel-recent-comment-1.third-top-commentator').css("display", "inline-block");
+			$('.carousel-recent-comment-2.third-top-commentator').css("display", "none");	
+		});
+	});
+
+	$(function() {
+		$('.top-review-wrapper .percent-off').mouseenter(function() {
+			$(this).hide('slow');
+		});
+		$('.top-review-wrapper .percent-off').mouseleave(function() {
+			$(this).show('slow');
+		});
+	});
+
+	/*поменять прозрачность кнопки плеера*/
+
 });
 
