b7edef4b421830c9c3ab28929a724f31a5672cf7
Vita Tkachenko <vita.tkachenko19@gmail.com>
2016-08-22 14:58:48 +0300

    add carousel plagin

--- learn-jquery.js
+++ learn-jquery.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	$('.article-image-presentation__title').mouseenter(function() {
 		$(this).addClass("change_color");
 	});
@@ -32,5 +33,156 @@ $(document).ready(function() {
 	$('a.film_roll_next').click(function(){
 		$('#carousel-wrapper').addClass('.simpleMove');
 	});
+
+	/*для первого комментария*/
+
+	$(function() {
+		$('.carousel-recent-comment-1.first-top-commentator').mouseenter(function(){
+			$(this).css("display", "none");
+			$(this).css("cursor", "pointer");
+			$('.carousel-recent-comment-2.first-top-commentator').css("cursor", "pointer");
+			$('.carousel-recent-comment-2.first-top-commentator').css("display", "inline-block");
+		});
+	});
+
+	$(function() {
+		$('.carousel-recent-comment-2.first-top-commentator').mouseleave(function() {
+			$('.carousel-recent-comment-1.first-top-commentator').css("display", "inline-block");
+			$('.carousel-recent-comment-2.first-top-commentator').css("display", "none");	
+		});
+	});
+
+	/*для второго комментария*/
+
+	$(function() {
+		$('.carousel-recent-comment-1.second-top-commentator').mouseenter(function(){
+			$(this).css("display", "none");
+			$(this).css("cursor", "pointer");
+			$('.carousel-recent-comment-2.second-top-commentator').css("cursor", "pointer");
+			$('.carousel-recent-comment-2.second-top-commentator').css("display", "inline-block");
+		});
+	});
+
+	$(function() {
+		$('.carousel-recent-comment-2.second-top-commentator').mouseleave(function() {
+			$('.carousel-recent-comment-1.second-top-commentator').css("display", "inline-block");
+			$('.carousel-recent-comment-2.second-top-commentator').css("display", "none");	
+		});
+	});
+
+	/*для третьего комментария*/
+
+	$(function() {
+		$('.carousel-recent-comment-1.third-top-commentator').mouseenter(function(){
+			$(this).css("display", "none");
+			$(this).css("cursor", "pointer");
+			$('.carousel-recent-comment-2.third-top-commentator').css("cursor", "pointer");
+			$('.carousel-recent-comment-2.third-top-commentator').css("display", "inline-block");
+		});
+	});
+
+	$(function() {
+		$('.carousel-recent-comment-2.third-top-commentator').mouseleave(function() {
+			$('.carousel-recent-comment-1.third-top-commentator').css("display", "inline-block");
+			$('.carousel-recent-comment-2.third-top-commentator').css("display", "none");	
+		});
+	});
+
+	$(function() {
+		$('.top-review-wrapper .percent-off').mouseenter(function() {
+			$(this).hide('slow');
+		});
+		$('.top-review-wrapper .percent-off').mouseleave(function() {
+			$(this).show('slow');
+		});
+	});
+
+	/*кнопки соц.сетей светлее по наведению и выделение курсором*/
+
+	$(function() {
+		$('.fa-rss-square').hover(function(){
+			$(this).addClass("lighten-rss-icon");
+			$(this).css("cursor", "pointer");
+		}, function() {
+			$(this).removeClass("lighten-rss-icon");
+			$(this).css("cursor", "none");
+		});
+	});
+
+	$(function() {
+		$('.fa-facebook-square').hover(function(){
+			$(this).addClass("lighten-facebook-icon");
+			$(this).css("cursor", "pointer");
+		}, function() {
+			$(this).removeClass("lighten-facebook-icon");
+			$(this).css("cursor", "none");
+		});
+	});
+
+	$(function() {
+		$('.fa-twitter-square').hover(function(){
+			$(this).addClass("lighten-twitter-icon");
+			$(this).css("cursor", "pointer");
+		}, function() {
+			$(this).removeClass("lighten-twitter-icon");
+			$(this).css("cursor", "none");
+		});
+	});
+
+	$(function() {
+		$('.fa-google-plus-square').hover(function(){
+			$(this).addClass("lighten-google-icon");
+			$(this).css("cursor", "pointer");
+		}, function() {
+			$(this).removeClass("lighten-google-icon");
+			$(this).css("cursor", "none");
+		});
+	});
+
+	/*вертикальні і горизонтальні лінії*/
+
+	$('.article-preview-img-wrapper').mouseenter(function(e) {
+		var thisElement = $(e.currentTarget);
+
+		thisElement.find('.vertical-lines').show('slow');
+		thisElement.find('.horizontal-lines').show('slow');
+		thisElement.find('.article-preview-img-wrapper__title').fadeIn(900);
+	});
+
+	$('.article-preview-img-wrapper').mouseleave(function() {
+		$('.vertical-lines').hide('normal');
+		$('.horizontal-lines').hide('normal');
+		$('.article-preview-img-wrapper__title').fadeOut(500);
+	});
+
+	/*колір заголовків*/
+
+	$('.article-preview-heading').hover(function () {
+		$(this).css("color", "#DADADA");
+		$(this).css("cursor", "pointer");
+	}, function() {
+		$(this).css("color", "#FFF");
+		$(this).css("cursor", "inherit");
+	});	
+
+	$('.sections-article-wrapper').mousemove(function (lines) {
+
+		var linesInThisBlock = $(lines.currentTarget);
+
+		linesInThisBlock.find('.thick-vertical-lines').show('fast');
+		linesInThisBlock.find('.thick-horizontal-lines').show('fast');
+		$(this).css("padding", "0 40px 0 40px");
+		$(this).css("transition", "0.5s ease-out");
+		$('.article-image-presentation__views').css("display", "none");
+	});
+
+	$('.sections-article-wrapper').mouseleave(function() {
+		$(this).css("padding", "0 35px 0 20px");
+		$('.thick-vertical-lines').hide('fast');
+		$('.thick-horizontal-lines').hide('fast');
+	});
+
+
+
 });
 
